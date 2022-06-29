66076b962fbc243f8dbc673f48d13f8649f9ccdb
Joaquin Orellana <joaquin.orellana@pucp.edu.pe>
2022-06-28 19:34:43 -0500

    modulos faltantes

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql2");
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const params = {
     host: "localhost",
@@ -19,6 +21,36 @@ conn.connect(function(err){
 //ACTIVIDAD 1
 
 //ACTIVIDAD 2
+
+app.post("/mascota/create", bodyParser.json(), (req, res) => {
+    let idmascota = req.body.idmascota;
+    let nombre = req.body.nombre;
+    let anho = req.body.anho;
+    let historia = req.body.historia;
+    let observaciones = req.body.observaciones;
+    let Sexo = req.body.Sexo;
+    let raza_especie_idraza = req.body.raza_especie_idraza;
+    let raza_otros = req.body.raza_otros;
+    let cuenta_idcuenta = req.body.cuenta_idcuenta;
+
+    let sql = "insert into mascota SET ?";
+    let params = {
+        idmascota: idmascota,
+        nombre:nombre,
+        anho: anho,
+        historia: historia,
+        observaciones: observaciones,
+        sexo: Sexo,
+        raza_especie_idraza: raza_especie_idraza,
+        raza_otros: raza_otros,
+        cuenta_idcuenta: cuenta_idcuenta
+    };
+
+
+
+});
+
+
 
 //ACTIVIDAD 3
 
