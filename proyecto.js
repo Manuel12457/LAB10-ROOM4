afd2caaea2fea959147dc6ff8dcc7ec4b200a903
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-06-28 20:10:04 -0500

    Avance ejercicio 3

--- proyecto.js
+++ proyecto.js
@@ -6,8 +6,8 @@ const multer = require("multer");
 
 const params = {
     host: "localhost",
-    user: "root",
-    password: "root",
+    user: "percy",
+    password: "percy",
     database: "sandylance"
 }
 
@@ -50,9 +50,22 @@ app.post("/mascota/create", bodyParser.json(), (req, res) => {
 
 });
 
-
-
 //ACTIVIDAD 3
+app.post('/servicio/create/:id', bodyParser.json(), function (req, res){
+
+    let idmascota = req.params.id
+
+    let parametros = [idmascota, req.body.cuenta_idcuenta, req.body.hora_inicio, req.body.duracion, req.body.entrega, req.body.responsable_idresponsable]
+    let sql = "insert into servicio (mascota_idmascota, cuenta_idcuenta, hora_inicio, duracion, entrega, responsable_idresponsable) values (?,?,?,?,?,?)"
+
+    conn.query(sql,parametros, function (err,result){
+        if (err) throw err
+
+        conn.query("select * from servicio order by idservicio desc", function (err, results) {
+            res.json(results)
+        })
+    })
+})
 
 //ACTIVIDAD 4
 
